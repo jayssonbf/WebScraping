c97094fcd261976805d313fd3ef0e6066937e6f8
jbalbuena <jayssonbf@outlook.com>
2022-11-16 16:37:37 -0500

    The software extracts the records of the last three(3) seasons for the FGCU women's soccer team

--- web_scrape.py
+++ web_scrape.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.select import Select
 import re
 
 """
@@ -14,31 +15,53 @@ if __name__ == '__main__':
     driver.get('https://fgcuathletics.com/sports/womens-soccer/stats?path=wsoc')
     assert "Page not found" not in driver.page_source
 
-    elem = driver.find_element(By.XPATH, "//*[@id='ui-id-3']")  # find Game-By-Game tab
-    elem.send_keys(Keys.ENTER)  # access Game-By-Game tab
+    for i in range(1, 4):
 
-    """
-    Explicit Waits - is a code you define to wait for a certain condition to occur before proceeding
-    further in the code. It waits 10secs until it goes to the next page or tab 
-    """
-    try:
-        table_head = WebDriverWait(driver, 10).until(
-            EC.presence_of_all_elements_located((By.XPATH, "//*[@id='DataTables_Table_4']/thead/tr/th"))
-        )
-        # ---------------COLUMNS------------------
-        columns = []
-        for head in table_head:
-            columns.append(head.text)
-        print(f'{columns}')
+        try:
+            # explicitly waits for the page to load to select the year
+            selects = Select(WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//select"))
+            ))
+            selects.select_by_index(i)
 
-        # ----------------ROWS-------------------
-        table_rowData = driver.find_elements(By.XPATH, "//*[@id='DataTables_Table_4']/tbody/tr/td")
-        rows = []
+            # explicitly waits for the page to load to click on the "Game-By-Game" tab
+            tab = WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//*[@id='ui-id-3']"))
+            )
+            tab.send_keys(Keys.ENTER)  # access Game-By-Game tab
 
-        for row in table_rowData:
-            rows.append(re.sub('\\n'," ", row.text))
+            """
+            Explicit Waits - is a code you define to wait for a certain condition to occur before proceeding
+            further in the code. It waits 10secs until it goes to the next page or tab
+            """
 
-        print(f'{rows}')
+            table_head = WebDriverWait(driver, 10).until(
+                EC.presence_of_all_elements_located((By.XPATH, "//*[@id='DataTables_Table_4']/thead/tr/th"))
+            )
 
-    except:
-        driver.quit()
+            year = driver.find_element(By.XPATH, "//*[@id='main-content']/article/h1")
+            year = "".join(re.findall('[\d]+', year.text))
+            print(f'Year {year}')
+
+            # ---------------COLUMNS------------------
+            columns = []
+            for head in table_head:
+                columns.append(head.text)
+            print(f'{columns}')
+
+            # ----------------ROWS-------------------
+            table_rowData = driver.find_elements(By.XPATH, "//*[@id='DataTables_Table_4']/tbody/tr")
+            rows = []
+
+            for row in table_rowData:
+                rows.append(re.sub('\\n'," ", row.text))
+
+            for n, row in enumerate(rows):
+                print(f'Row {n+1}: {row}')
+            print("")
+
+        except:
+            print("Something went wrong when loading the page")
+            driver.quit()
+
+    driver.quit()
