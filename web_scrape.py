efdd447f871d9ed8b5996544207a18bb3d832596
jbalbuena <jayssonbf@outlook.com>
2022-11-16 20:54:31 -0500

    The program stores in a file the statistics of the FGCU women's soccer team for the last three seasons and prints the win-loss record for each season and shows the season in which they did best

--- web_scrape.py
+++ web_scrape.py
@@ -6,6 +6,62 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 import re
 
+
+def create_file(table_head, table_body):
+
+    title = driver.find_element(By.XPATH, "//*[@id='main-content']/article/h1").text
+    year = "".join(re.findall('[\d]+', title))
+
+    try:
+        # File creation
+        f = open(f'{year}_sport.txt', 'a')
+        try:
+            f.write(f'{title}\n\n')
+            f.write(table_head.text + '\n')
+
+            for row in table_body:
+                f.write(re.sub('\\n', " ", row.text) + '\n')
+
+        except:
+            print("Something went wrong when writing to the file")
+
+        finally:
+            f.close()
+            return year
+
+    except:
+        print("Something went wrong when opening the file")
+
+
+def display_record(table_body, year):
+
+    wins = []
+    losses = []
+    draws = []
+
+    for row in table_body:
+        match = re.search("\s[W|L|T]\s", row.text)
+        if match:
+            char = (match.group()).strip()
+            if char == 'W':
+                wins.append(char)
+            elif char == 'L':
+                losses.append(char)
+            else:
+                draws.append(char)
+
+    w = len(wins)
+    l = len(losses)
+    d = len(draws)
+    wpct = ((w + (d * .5)) / ((w + (d * .5)) + (l + (d * .5))))
+
+    # display team win-loss record
+    print('\n{:>25} {}\n'.format('Team Win-Loss Record', year))
+    print('{:^10}{:^10}{:^10}{:>5}\n{:^10}{:^10}{:^10}{:6.3f}'.format(
+        'Wins', 'Losses', 'Draws', 'Win%', w, l, d, wpct))
+
+    return wpct
+
 """
 Basic overview of selenium over BeautifulSoup for web scrapping.
 """
@@ -14,6 +70,8 @@ if __name__ == '__main__':
     driver = webdriver.Chrome()
     driver.get('https://fgcuathletics.com/sports/womens-soccer/stats?path=wsoc')
     assert "Page not found" not in driver.page_source
+    wpct = {}
+    year = ""
 
     for i in range(1, 4):
 
@@ -36,32 +94,22 @@ if __name__ == '__main__':
             """
 
             table_head = WebDriverWait(driver, 10).until(
-                EC.presence_of_all_elements_located((By.XPATH, "//*[@id='DataTables_Table_4']/thead/tr/th"))
+                EC.presence_of_element_located((By.XPATH, "//*[@id='DataTables_Table_4']/thead/tr"))
             )
 
-            year = driver.find_element(By.XPATH, "//*[@id='main-content']/article/h1")
-            year = "".join(re.findall('[\d]+', year.text))
-            print(f'Year {year}')
+            table_body = driver.find_elements(By.XPATH, "//*[@id='DataTables_Table_4']/tbody/tr")
 
-            # ---------------COLUMNS------------------
-            columns = []
-            for head in table_head:
-                columns.append(head.text)
-            print(f'{columns}')
+            year = create_file(table_head, table_body)
 
-            # ----------------ROWS-------------------
-            table_rowData = driver.find_elements(By.XPATH, "//*[@id='DataTables_Table_4']/tbody/tr")
-            rows = []
-
-            for row in table_rowData:
-                rows.append(re.sub('\\n'," ", row.text))
-
-            for n, row in enumerate(rows):
-                print(f'Row {n+1}: {row}')
-            print("")
+            wpct[year] = display_record(table_body, year) #Win pct
 
         except:
             print("Something went wrong when loading the page")
             driver.quit()
 
     driver.quit()
+
+    best_season = {year for year in wpct if wpct[year] == max(wpct.values())}
+    print(f"\nThe team's best season was in {''.join(best_season)}")
+
+
